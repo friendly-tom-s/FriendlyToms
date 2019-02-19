212709aa12160973ffb552f876392ce85b660865
Tomh99 <Tomh99@users.noreply.github.com>
2019-02-19 11:57:05 +0000

    Added in a comment

--- AppSwing/AppSwing/src/com/codebind/credential.java
+++ AppSwing/AppSwing/src/com/codebind/credential.java
@@ -9,6 +9,7 @@ public class credential {
     hash and salt password (hash_salt_password)
     verify credentials (authenticate_user)
     update_credentials / change username / change password?
+    Good Ideas
 
 
 
