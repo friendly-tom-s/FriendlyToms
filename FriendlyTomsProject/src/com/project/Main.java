e3d6dcaccc886702c816c481bec40ade0de51ffe
jb8699 <47359800+jb8699@users.noreply.github.com>
2019-02-25 09:17:08 +0000

    Please read and leave comments throughout

--- FriendlyTomsProject/src/com/project/Main.java
+++ FriendlyTomsProject/src/com/project/Main.java
@@ -2,6 +2,6 @@ package com.project;
 
 public class Main {
     public static void main(String[] args) {
-
+        //This is the main project file. If we could stick to branching off this to maintain integrity that would be good.
     }
 }
