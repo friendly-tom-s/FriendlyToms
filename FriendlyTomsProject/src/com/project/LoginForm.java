06ca353bac55835fbf8bbdb80bd4e1a24eb9952b
oldfield1998 <34140862+oldfield1998@users.noreply.github.com>
2019-02-26 12:17:37 +0000

    Login form created

---
+++ FriendlyTomsProject/src/com/project/LoginForm.java
@@ -0,0 +1,11 @@
+package com.project;
+
+import javax.swing.*;
+
+public class LoginForm {
+    private JPanel panel1;
+    private JButton btnLogin;
+    private JButton btnCreate;
+    private JTextField txtUsername;
+    private JPasswordField txtPassword;
+}
