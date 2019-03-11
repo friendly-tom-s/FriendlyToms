83326b9054c5cf5a7f6e6701a4e52f8c02ee61bf
oldfield1998 <34140862+oldfield1998@users.noreply.github.com>
2019-03-11 13:26:16 +0000

    Checkout Complete

---
+++ FriendlyTomsProject/src/com/project/Checkout.java
@@ -0,0 +1,16 @@
+package com.project;
+
+import javax.swing.*;
+
+public class Checkout {
+    private JList lstUnit;
+    private JList lstPrice;
+    private JLabel lblPrice;
+    private JComboBox cboTable;
+    private JTextField txtCVV;
+    private JTextField txtCardNumber;
+    private JTextField txtName;
+    private JComboBox cboExpiry;
+    private JButton btnPay;
+    private JButton btnBack;
+}
