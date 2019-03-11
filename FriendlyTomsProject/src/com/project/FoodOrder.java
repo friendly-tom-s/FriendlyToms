2548509f83caf108ba2d9d05b394608d7e7d5794
oldfield1998 <34140862+oldfield1998@users.noreply.github.com>
2019-03-11 13:03:58 +0000

    Food Order GUI

---
+++ FriendlyTomsProject/src/com/project/FoodOrder.java
@@ -0,0 +1,13 @@
+package com.project;
+
+import javax.swing.*;
+
+public class FoodOrder {
+    private JComboBox cboStarter;
+    private JPanel panel1;
+    private JComboBox cboMain;
+    private JComboBox cboDessert;
+    private JComboBox cboDrink;
+    private JButton btnOrder;
+    private JButton btnMenu;
+}
