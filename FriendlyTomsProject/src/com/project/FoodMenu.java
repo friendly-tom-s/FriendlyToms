d626e3ede7c3d483a2cbd1565e4e1ad7cacd97c6
oldfield1998 <34140862+oldfield1998@users.noreply.github.com>
2019-03-11 13:08:41 +0000

    Menu GUI

---
+++ FriendlyTomsProject/src/com/project/FoodMenu.java
@@ -0,0 +1,12 @@
+package com.project;
+
+import javax.swing.*;
+
+public class FoodMenu {
+    private JList lstBreakfast;
+    private JPanel panel1;
+    private JList lstMainMenu;
+    private JList lstDessert;
+    private JList lstDrinks;
+    private JButton btnMenu;
+}
