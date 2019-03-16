937a17175c15de3f1d4926562886d6cd2aa9ec8f
jb8699 <47359800+jb8699@users.noreply.github.com>
2019-03-16 00:04:38 +0000

    New Merge

---
+++ FriendlyTomsProject/src/com/project/TableBooking.java
@@ -0,0 +1,12 @@
+package com.project;
+
+import javax.swing.*;
+
+public class TableBooking {
+    private JTextField txtName;
+    private JComboBox cboDate;
+    private JComboBox cboTime;
+    private JComboBox cboPeople;
+    private JButton btnBook;
+    private JButton btnMainMenu;
+}
