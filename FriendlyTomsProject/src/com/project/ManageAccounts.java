310156d8ea56470551fc3bbc1a9e398ea5b27909
oldfield1998 <34140862+oldfield1998@users.noreply.github.com>
2019-03-19 10:15:25 +0000

    New GUI Forms

---
+++ FriendlyTomsProject/src/com/project/ManageAccounts.java
@@ -0,0 +1,10 @@
+package com.project;
+
+import javax.swing.*;
+
+public class ManageAccounts {
+    private JButton btnView;
+    private JButton btnDelete;
+    private JButton btnCreate;
+    private JButton btnMenu;
+}
