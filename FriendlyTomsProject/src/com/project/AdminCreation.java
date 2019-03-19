9bde73ac5e65d34afc4883ce17182a7fc45ae8ba
oldfield1998 <34140862+oldfield1998@users.noreply.github.com>
2019-03-19 10:35:04 +0000

    New GUIs created

---
+++ FriendlyTomsProject/src/com/project/AdminCreation.java
@@ -0,0 +1,11 @@
+package com.project;
+
+import javax.swing.*;
+
+public class AdminCreation {
+    private JTextField btnName;
+    private JTextField btnUsername;
+    private JTextField btnPassword;
+    private JRadioButton rdoAdmin;
+    private JButton btnMenu;
+}
