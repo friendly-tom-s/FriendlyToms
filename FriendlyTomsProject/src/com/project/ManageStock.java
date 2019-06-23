ac0ca5f9bf4b26bd87c4d6d39998257b8f47d9ac
jb8699 <47359800+jb8699@users.noreply.github.com>
2019-06-23 12:32:08 +0100

    Added the stock update and view

--- FriendlyTomsProject/src/com/project/ManageStock.java
+++ FriendlyTomsProject/src/com/project/ManageStock.java
@@ -1,5 +1,6 @@
 package com.project;
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -14,46 +15,45 @@ import java.util.ArrayList;
 public class ManageStock extends TemplateGui {
     private JPanel panel2;
     private JComboBox cboItem;
+    DefaultTableModel model = new DefaultTableModel();
     private JTextField txtAddStock;
     private JButton btnAddStock;
     private JLabel labAmount;
     private JButton btnAddMenuItem;
+    private JTable table;
+    private JScrollPane pane;
+    private JButton btnUpdate;
     private ArrayList<String> main_items = new ArrayList<String>();
+    private JPanel panelMain = new JPanel(new BorderLayout());
 
     public ManageStock() {
         super("Manage Stock", "Main Menu", "AdminMenu");
+
     }
 
     /**
      * This is the typical method of adding action listeners and Gui settings.
      */
     public void DisplayStock() {
-        frame.add(panel2, BorderLayout.CENTER);
+        setJtable();
+        Dimension dimension = new Dimension();
+        dimension.setSize(500, 275);
+        table.setPreferredScrollableViewportSize(dimension);
+        pane = new JScrollPane(table);
+        panelMain.add(pane, BorderLayout.CENTER);
+        panelMain.add(btnUpdate, BorderLayout.SOUTH);
+
+        frame.add(panelMain, BorderLayout.CENTER);
         DisplayGenericElements();
-        setInitialStock();
 
-        btnAddMenuItem.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                AddMenuItem addMenuItem = new AddMenuItem();
-                addMenuItem.DisplayAddMenuItem();
-                frame.dispose();
-            }
-        });
-
-        cboItem.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                updateStockAmountOnPress();
-            }
-        });
-
-        btnAddStock.addActionListener(new ActionListener() {
+        btnUpdate.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 updateStock();
             }
         });
+
+
     }
 
     /**
@@ -62,37 +62,49 @@ public class ManageStock extends TemplateGui {
      * It will update the stock with the new amount.
      */
     public void updateStock(){
-        boolean write_query = database.prepared_write_query("UPDATE menu SET stock =? WHERE name = ?", txtAddStock.getText(), (cboItem.getSelectedItem()).toString());
-    }
 
-    /**
-     * When the class is called this method is run to update the dropdown list with all the food items.
-     */
-    public void setInitialStock(){
-        ResultSet stockItems = database.prepared_read_query("SELECT * FROM menu");
-        try {
-            while (stockItems.next()) {
-                String name = stockItems.getString("name");
-                cboItem.addItem(name);
+            int dialogButton = JOptionPane.YES_NO_OPTION;
+            int dialogResult = JOptionPane.showConfirmDialog(null,
+                    "Are you sure you wish to update the changed items?", "Update Stock confirmation", dialogButton);
+            if (dialogResult == 0) {
+                databaseLogic();
             }
-        } catch (SQLException e) {
-            e.printStackTrace();
 
-        }
     }
 
-    /**
-     * When a food item is selected this gets and updates the amount of stock in the label.
-     */
-    public void updateStockAmountOnPress(){
-        String dbItem = (cboItem.getSelectedItem()).toString();
-        ResultSet matchingItem = database.prepared_read_query("SELECT stock FROM menu WHERE name =?", dbItem);
-        try {
-            while (matchingItem.next()) {
-                labAmount.setText(String.valueOf(matchingItem.getInt("stock")));
+    public void databaseLogic(){
+        int row = table.getSelectedRow();
+        String nameVar = table.getValueAt(row, 0).toString();
+        int stockVar = Integer.parseInt(table.getValueAt(row, 1).toString());
+        boolean write_query = database.prepared_write_query("UPDATE menu SET stock =? WHERE name = ?",stockVar, nameVar );
+        setJtable();
+    }
+
+    public void setJtable(){
+
+        Object[] columns = {"Food", "Stock"};
+        model.setColumnIdentifiers(columns);
+        ResultSet read_query = database.prepared_read_query("SELECT * FROM menu");
+
+        try
+        {
+            while (read_query.next())
+            {
+                String foodName = read_query.getString("name");
+                String stock = read_query.getString("stock");
+                String user_info[] = {foodName, stock};
+                model.addRow(user_info);
             }
-        } catch (SQLException e1) {
-            e1.printStackTrace();
         }
+        catch (Exception e)
+        {
+            System.err.println("Got an exception! EXITING ");
+            System.err.println(e.getMessage());
+            System.exit(1);
+        }
+        // set the model to the table
+        table.setModel(model);
+
     }
+
 }
